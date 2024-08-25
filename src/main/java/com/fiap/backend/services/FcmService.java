e5f0b785d864ab672a5872c2ed556132dac526a6
klleriston <klleandrade@outlook.com>
2024-08-25 16:22:37 -0300

    feat: add firebase integration

---
+++ src/main/java/com/fiap/backend/services/FcmService.java
@@ -0,0 +1,25 @@
+package com.fiap.backend.services;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.Message;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FcmService {
+
+    public void sendNotification(String token, String title, String body) {
+        Message message = Message.builder()
+                .setToken(token)
+                .putData("title", title)
+                .putData("body", body)
+                .build();
+
+        try {
+            String response = FirebaseMessaging.getInstance().send(message);
+            System.out.println("Successfully sent message: " + response);
+        } catch (Exception e) {
+            System.out.println("Failed to send message: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
